afb68e853e4fc5acfaaac65362bb3bc1903e6a43
suchada <su.chavengpoj@gmail.com>
2014-03-10 18:53:38 +0700

    Galen with Home login case
    This case is not complete because it cannot load galenjs.js.

---
+++ HealthNow/GalenTestScript/HomeLogin/homelogin.js
@@ -0,0 +1,17 @@
+// Include the GalenJs library
+load("galenjs.js");
+
+// Create a GalenJs instance from driver
+var $ = GalenJs.create(driver);
+
+// Create a login page object
+var homeLogin = $.page({
+    username:      "#txtUsername",
+    password:   "#txtPassword",
+    login:     "#btnLogin"
+});
+
+// Perform login flow
+homeLogin.username.typeText("papajohn_jr");
+homeLogin.password.typeText("1981papajohn_jr");
+homeLogin.login.click();
