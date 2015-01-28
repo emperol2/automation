fb52afc7c8e3c1898f4da4bffad7beba0ba0beec
suchada <su.chavengpoj@gmail.com>
2015-01-28 12:19:38 +0700

    Update search test script for BK_NZ

--- BK_NZ/search_test.rb
+++ BK_NZ/search_test.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require 'rubygems'
+#require 'rubygems'
 require 'selenium-webdriver'
 
 class NzSearchTest < Test::Unit::TestCase
@@ -10,36 +10,50 @@ class NzSearchTest < Test::Unit::TestCase
     # Do nothing
     @driver = Selenium::WebDriver.for :firefox
     @driver.manage.window.maximize
-    @wait = Selenium::WebDriver::Wait.new(timeout: '20')
-    @nz_url = 'http://burgerking.co.nz/'
-    @driver.get(@nz_url)
+
+    @driver.manage.timeouts.implicit_wait = 300
+    @wait = Selenium::WebDriver::Wait.new(timeout: 20)
+    @nz_staging = 'http://ec2-54-85-128-13.compute-1.amazonaws.com'
+    @nz_live = 'http://burgerking.co.nz/'
+    @driver.get @nz_staging
   end
 
   # Called after every test method runs. Can be used to tear
   # down fixture information.
 
-  def test_search_found
+  def test_sitesearch_found
+    @driver.find_element(:css, 'li.hasForm.searchForm span.menuItem-medium').click
+    assert @wait.until {@driver.find_element(:css, 'li.hasForm.searchForm form.formArea input.navInput').displayed?}
+    @driver.find_element(:css, 'li.hasForm.searchForm form.formArea input.navInput').send_key('Chicken Nuggets')
+    @driver.find_element(:css, 'li.hasForm.searchForm form.formArea button.navInputSubmit').click
     p @driver.title
-    @driver.find_element(css: 'li.hasForm.searchForm span.menuItem-medium').click
-    @driver.find_element(css: 'li.hasForm.searchForm form.formArea input.navInput').send_key('Chicken Nuggets')
-    @driver.find_element(css: 'li.hasForm.searchForm form.formArea button.navInputSubmit').click
     assert @driver.title.include?('Search')
-    p @driver.find_element(css: 'section.container-fluid.searchItems div.resultsNo').text
-    assert_raise do
-      @wait.until {@driver.find_element(tag_name: 'img')}
-      @wait.until {@driver.find_element(css: 'div.itemDetails')}
-    end
+    p @driver.find_element(:css, 'section.container-fluid.searchItems div.resultsNo').text
+    assert_not_nil @driver.find_element(:tag_name, 'img')
+    assert_not_nil @driver.find_element(:css, 'div.itemDetails')
   end
 
-  def test_search_not_found
+  def test_sitesearch_not_found
+    @driver.find_element(:css, 'li.hasForm.searchForm span.menuItem-medium').click
+    assert @wait.until {@driver.find_element(:css, 'li.hasForm.searchForm form.formArea input.navInput').displayed?}
+    @driver.find_element(:css, 'li.hasForm.searchForm form.formArea input.navInput').send_key('Sandwich')
+    @driver.find_element(:css, 'li.hasForm.searchForm form.formArea button.navInputSubmit').click
     p @driver.title
-    @driver.find_element(css: 'li.hasForm.searchForm span.menuItem-medium').click
-    @driver.find_element(css: 'li.hasForm.searchForm form.formArea input.navInput').send_key('Sandwich')
-    @driver.find_element(css: 'li.hasForm.searchForm form.formArea button.navInputSubmit').click
     assert @driver.title.include?('Search')
-    p @driver.find_element(css: 'section.container-fluid.searchItems div.resultsNo').text
-    #assert @driver.element_present?(tag_name: 'img')
-    #assert @driver.element_present?(css: 'div.itemDetails')
+    p @driver.find_element(:css, 'section.container-fluid.searchItems div.resultsNo').text
+    assert element_present?(:css, 'h3.sectionTitle')
+    assert @driver.find_element(:css, 'div.resultsNo').text.include?'No results found'
+  end
+
+  def test_search
+    assert @wait.until {@driver.find_element(:css, 'li.hasForm.searchForm span.menuItem-medium i.icon').displayed?}
+    @driver.find_element(:css, 'li.hasForm.searchForm span.menuItem-medium i.icon').click
+    @driver.find_element(:css, 'li.hasForm.searchForm form.formArea button.navInputSubmit').click
+    assert @driver.title.include?'Search'
+    assert @driver.find_element(:css, 'div.resultsNo').text.include?'No results found'
+    @driver.find_element(:id, 'edit-term').send_key('Cheese')
+    @driver.find_element(:css, 'form#views-exposed-form-search-page button.submit').click
+    assert @driver.find_element(:css, 'section.container-fluid.searchItems ul.itemsList').text.include?'Cheese'
   end
 
   def element_present?(how, what)
