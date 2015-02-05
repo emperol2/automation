f36697ba1545e105d006d4a8b72b7b4f20414b19
suchada <su.chavengpoj@gmail.com>
2015-02-05 16:05:34 +0700

    Update search test script

--- BK_NZ/fresh_offer_test.rb
+++ BK_NZ/fresh_offer_test.rb
@@ -24,20 +24,30 @@ class NzFreshOfferTest < Test::Unit::TestCase
   def test_offers
     assert @driver.title.include?'Offers'
     # Fresh Offer banner
-    assert @driver.find_element(:tag_name, 'img')
+    assert element_present?(:tag_name, 'img')
+    assert_true @driver.page_source.include? "http://burgerking.co.nz/sites/default/files/FreshOffer_Coupon.jpg"
   end
 
   def test_right_left_image
     assert @driver.title.include?'Offers'
-    # Right image
-    assert @driver.find_element(:css, 'div.right-image')
-    # Left image
-    assert @driver.find_element(:css, 'div.left-image')
+    # Verify right image
+    assert element_present?(:css, 'div.right-image')
+    assert_true @driver.page_source.include? "background-image:url('http://burgerking.co.nz/sites/default/files/Panel_Fries_L.jpg')"
+    # Verify left image
+    assert element_present?(:css, 'div.left-image')
+    assert_true @driver.page_source.include? "background-image:url('http://burgerking.co.nz/sites/default/files/Panel_Fries_L.jpg')"
   end
 
   def test_crm_block
     assert @driver.title.include?'Offers'
-    assert element_present?(:css, 'section.container-fluid crm')
+    assert @driver.find_element(:css, 'section.container-fluid.crm')
+    #@crm_block = @driver.find_element(:css, 'section.container-fluid.crm')
+    #@crm_block.location_once_scrolled_into_view
+    assert element_present?(:css, 'section.container-fluid.crm')
+    assert element_present?(:css, 'div.col-sm-6.special-stores')
+    assert @driver.find_element(:css, 'div.col-sm-6.special-stores div.crmTitle span').text.include? 'Time Out Entertainment Zone'
+    assert element_present?(:css, 'div.col-sm-6')
+    assert @driver.find_element(:css, 'div.col-sm-6 div.crmText').text.include? 'JOIN THE BK'
   end
 
   def element_present?(how, what)
